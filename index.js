c8d0ee6daf3745c21eeb44e31980d57beb9c19ae
ShamimKhan2006 <mdshamim200616@gmail.com>
2026-06-20 16:06:45 +0600

    new featured section added

--- index.js
+++ index.js
@@ -7,10 +7,74 @@ const port = 8000
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+ 
+// const { ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri =process.env.MONGODB_URL
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+      const database = client.db("legalease");
+    const lawyersColl= database.collection("laywersData");
+    const topLawyers=database.collection("topLawyers")
+    
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+      app.get("/lawyers",async (req,res)=>{
+        const query=req.query
+        const result=await lawyersColl.find(query).toArray()
+        res.send(result)
+
+      })
+
+      app.get("/lawyers/featured",async (req,res)=>{
+        const query=req.query
+        const result=await lawyersColl.find(query).limit(6).toArray()
+        res.send(result)
+      })
+
+
+   app.get("/lawyers/top",async (req,res)=>{
+    const query=req.query
+    const result=await topLawyers.find(query).sort({hires:-1}).limit(3).toArray()
+    res.send(result)
+   })
+
+
+   app.get("/lawyers/:id",async (req,res)=>{
+    const {id}=req.params
+    const lawyer=await lawyersColl.findOne({
+      _id:new ObjectId(id)
+    })
+
+     res.send(lawyer)
+
+   })
+
+ 
+
+
+
+      app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
